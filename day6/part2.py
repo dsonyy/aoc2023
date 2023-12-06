6a7c91a418d5cfe1378401c159eeb68908991608
Szymon Bednorz <dsonyyy@gmail.com>
2023-12-06 16:29:37 +0100

    day6

---
+++ day6/part2.py
@@ -0,0 +1,17 @@
+with open("input") as f:
+    times = [int("".join(f.readline().split()[1:]))]
+    dists = [int("".join(f.readline().split()[1:]))]
+
+result = 1
+for i_race in range(len(times)):
+    time = times[i_race]
+    dist = dists[i_race]
+
+    cnt = 0
+    for t in range(0, time + 1):
+        score = (time - t) * t
+        if score > dist:
+            cnt += 1
+
+    result *= cnt
+print(result)

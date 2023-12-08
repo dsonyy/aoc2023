cb6bcf2a5f56b2df404096646c6f7a9e6ead81f8
Szymon Bednorz <dsonyyy@gmail.com>
2023-12-08 15:22:47 +0100

    day5 part2

--- day5/part2.py
+++ day5/part2.py
@@ -2,7 +2,6 @@ import re
 
 ranges = []
 maps = [[] for _ in range(7)]
-inters = set([])
 
 with open("input") as f:
     mapi = -1
@@ -21,46 +20,21 @@ with open("input") as f:
 
         dst, src, n = [int(v) for v in line.split()]
         maps[mapi].append((dst, src, n))
-        inters.add(src)
-        inters.add(src+n+1)
-        inters.add(dst)
-        inters.add(dst+n+1)
-
-ranges2 = []
-for range_ in ranges:
-    start, n = range_
-    inters2 = []
-    for inter in inters:
-        if start <= inter and inter < start+n:
-            inters2.append(inter)
-
-    if len(inters2) == 0:
-        ranges2.append((start, n))
-    else:
-        for i in range(len(inters2)):
-            if i == 0:
-                ranges2.append((start, inters2[i] - start))
-            else:
-                ranges2.append((inters2[i-1], inters2[i] - inters2[i-1]))
-        ranges2.append((inters2[-1], start+n - inters2[-1]))
 
 
-print(ranges)
-print(inters)
-# print(ranges2)
+def f():
+    for dstt in range(46, 10000000000):
+        pos = dstt
+        for map in maps[::-1]:
+            for record in map:
+                dst, src, n = record
+                if dst <= pos and pos < dst + n:
+                    pos -= dst - src
+                    break
 
-seeds = [r[0] for r in ranges2]
-# print(seeds)
+        for range_ in ranges:
+            if range_[0] <= pos and pos < range_[0] + range_[1]:
+                return dstt
 
-poses = []
-for seed in seeds:
-    pos = seed
-    for map in maps:
-        for record in map:
-            dst, src, n = record
-            if src <= pos and pos < src + n:
-                pos += dst - src
-                break
-    poses.append(pos)
-print(min(poses))
-# 6472060
+
+print(f())

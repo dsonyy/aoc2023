b6b314a5cfe0d5f6c38482eb29662bcb5a0efdf6
Szymon Bednorz <dsonyyy@gmail.com>
2023-12-09 11:38:17 +0100

    day9

---
+++ day9/part2.py
@@ -0,0 +1,21 @@
+def d(series):
+    return [b - a for a, b in zip(series, series[1:])]
+
+
+def estimate(hist, row=0):
+    if row == len(hist) - 1:
+        return 0
+    return hist[row][0] - estimate(hist, row + 1)
+
+
+with open("input") as f:
+    data = [[int(n) for n in line.split()] for line in f.readlines()]
+
+    sum = 0
+    for series in data:
+        hist = [series]
+        while any(h != 0 for h in hist[-1]):
+            hist.append(d(hist[-1]))
+        sum += estimate(hist)
+
+    print(sum)
